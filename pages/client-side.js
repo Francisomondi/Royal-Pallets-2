814640c0f2139f7cb0d40e5dcf9227c9a83f5745
francis omondi <francisomondi17@gmail.com>
2023-03-06 16:10:09 +0300

    client side

---
+++ pages/client-side.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const clientSide = () => {
+return (
+    <div>
+    client side
+    </div>
+)
+}
+
+export default clientSide
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function HomePage() {
 return (
     <div>
-    <h1>Welcome to Next.js!</h1>
+    <h1>Welcome to Royal Pallets & Interior decor!</h1>
     </div>
     )
 }
