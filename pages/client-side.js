b98984b1c6f7245033d71663359172c7c099a891
francis omondi <francisomondi17@gmail.com>
2023-03-07 23:11:36 +0300

    fetching data from an api

--- pages/client-side.js
+++ pages/client-side.js
@@ -1,11 +1,54 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
+const url ='https://pokeapi.co/api/v2/pokemon?limit=151' 
 
 const clientSide = () => {
-return (
+
+    const [pokemon, setPokemon] = useState([])
+
+
+    useEffect(()=>{
+        const fetchPokemon = async()=>{
+        const response= await axios.get(url)
+        
+        const promises = response.data.results.map(result => {
+          return(axios.get(result.url))
+        });
+
+        const responses = await Promise.all(promises)
+        console.log(responses)
+       
+
+        const pokeData= responses.map(r=>{
+            return{
+                name: r.data.name,
+                imgUrl: r.data.sprites.front_default
+            }
+
+        })
+
+        setPokemon(pokeData)
+
+
+        }
+
+        fetchPokemon()
+    },[])
+
+    
+
+    return pokemon.map(poke=>{
+        return (
     <div>
-    client side
+    <img src={poke.imgUrl} />
+    <p>{poke.name}</p>
+    <hr/>
     </div>
 )
+
+    })
+
 }
 
 export default clientSide
